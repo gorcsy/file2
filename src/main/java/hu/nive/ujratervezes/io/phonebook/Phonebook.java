e7f59f1819ce5e13329fdd7937391c66256a04ce
Görcs Balázs <gorcs.balazs@gmail.com>
2021-04-29 11:03:28 +0200

    all tests green

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,24 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+public class Phonebook {
+
+    public void exportPhonebook(Map<String, String> contacts, String output) {
+        if (contacts == null || output == null) {
+            throw new IllegalArgumentException();
+        }
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(output))) {
+            for (Map.Entry<String, String> entry : contacts.entrySet()) {
+                String name = entry.getKey();
+                String phoneNumber = entry.getValue();
+                writer.write(name + ": " + phoneNumber + "\n");
+            }
+        } catch (IOException ex) {
+            throw new IllegalArgumentException(ex);
+        }
+    }
+}
